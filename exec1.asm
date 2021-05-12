2ae746d3554dc45d4961fb4db7d003eb708a01a2
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2021-05-12 13:44:52 -0300

    Exercício 1

---
+++ exec1.asm
@@ -0,0 +1,8 @@
+.text
+ori $t1, $zero, 527 #t1 = 527
+addi $t2, $zero, -372 #t2 = -372
+ori $t3, $zero, 225 #t3 = 225
+addi $t4, $zero, -794 #t4 = -794
+add $t0, $t1, $t2 #t0 = 527 + (-372) = 155
+add $t0, $t0, $t3 #t0 = 155 + 225 = 380
+add $t0, $t0, $t4 #t0 = 380 + (-794) = -414 | Resultado
